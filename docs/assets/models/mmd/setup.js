11f409dac82a433928d179881085a95bca521bb3
jomin398 <jomin398@gmail.com>
2021-07-05 12:30:43 +0900

    update!

---
+++ docs/assets/models/mmd/setup.js
@@ -0,0 +1,30 @@
+const mmdsetUpData = {
+    miku_v2: {
+        floorOffset: [0, 0, 0],
+        chrOffset: [0, 0, 0]
+    },
+    ちびミク: {
+        floorOffset: [0, 5, 0],
+        chrOffset: [0, 5, 0]
+    },
+    songParams:{
+        wavefile:{
+            fname:"wavefile_shorts",
+            vmd:"/wavefile_v2.vmd",
+            cam:"/wavefile_camera.vmd",
+            song:"/wavefile_short.mp3",
+            author:null,
+            artist:"初音ミク",
+            delayTime:5.33
+        },
+        Rough:{
+            fname:"GFRIEND_Rough",
+            vmd:"/GFRIEND_Rough.vmd",
+            cam:"/GFRIEND_Rough_Camera.vmd",
+            song:"/GFRIEND_Rough.wav",
+            author:null,
+            artist:"여자친구",
+            delayTime:0
+        }
+    }
+}
